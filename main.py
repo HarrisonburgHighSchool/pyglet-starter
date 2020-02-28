ec091fa3574d2b118e229de48745058aff6f5154
Jon Stapleton <jstapleton@harrisonburg.k12.va.us>
2020-02-28 13:22:01 -0500

    hjde

--- main.py
+++ main.py
@@ -3,20 +3,26 @@ from util import *
 
 win = pyglet.window.Window()
 # Load the image & create the sprite
-img = pyglet.image.load('assets/hero/Old hero.png')
+img = pyglet.image.load('assets/hero/sliced/idle-1.png')
 spr = pyglet.sprite.Sprite(img, x = 100, y = 200)
+spr.scale = 4
+
+speed = -1
 
 
 def update(dt):
-  pass
+  global speed
+  spr.x += speed
+  if spr.x < 100 or spr.x > 400:
+    speed = speed * -1
 
 @win.event
 def on_draw():
   win.clear()
-  # pixelScale()
+  pixelScale()
 
   spr.draw()
 
-pyglet.clock.schedule(update)
+pyglet.clock.schedule_interval(update, 0.015)
 pyglet.app.run()
 
